274ab92b888e856f9e34aeb4214277061f7f4154
Akshat Agrawal <agrawalakshat582@gmail.com>
2024-11-20 17:54:53 +0530

    tp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import "cal-sans";
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Intro from "./Components/Intro/Intro";
 import Welcome from "./Components/Welcome/Welcome";
 import WhatWeDo from "./Components/WhatWeDo/WhatWeDo";
 import ContactUs from "./Components/ContactUs/ContactUs";
 import Footer from "./Components/Footer/Footer";
+import SubmitProfile from "./Components/SubmitProfile/SubmitProfile";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Intro/>
-    <Welcome/>
-    <WhatWeDo/>
-    <ContactUs/>
-    <Footer/>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Intro />
+            <Welcome />
+            <WhatWeDo />
+            <ContactUs />
+            <Footer />
+          </>
+        } />
+        <Route path="/submit-profile" element={<SubmitProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/WhatWeDo/WhatWeDo.js
+++ src/Components/WhatWeDo/WhatWeDo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./WhatWeDo.css";
+import { Link } from 'react-router-dom';
 
 // Image Imports (Update the paths to match your setup)
 import ActingImg from "../../Images/acting.png";
@@ -15,30 +16,54 @@ const WhatWeDo = () => {
       <h2 className="what-we-do-title">What we do</h2>
       <div className="grid-container">
         {/* Grid Items */}
+      
         <div className="grid-item acting">
+        <Link to="/submit-profile" className="submit-link">
           <img src={ActingImg} alt="Acting" />
           <span className="grid-text">ACTING</span>
+          </Link>
         </div>
+  
+        
         <div className="grid-item writing">
+        <Link to="/submit-profile" className="submit-link">
           <img src={WritingImg} alt="Writing" />
           <span className="grid-text">WRITING</span>
+          </Link>
         </div>
+  
+      
         <div className="grid-item production">
+        <Link to="/submit-profile" className="submit-link">
           <img src={ProductionManagingImg} alt="Production Managing" />
           <span className="grid-text">PRODUCTION MANAGING</span>
+          </Link>
         </div>
+
+        
         <div className="grid-item direction">
+        <Link to="/submit-profile" className="submit-link">
           <img src={DirectionImg} alt="Direction" />
           <span className="grid-text">DIRECTION</span>
+          </Link>
         </div>
+       
+     
         <div className="grid-item filmmaking">
+        <Link to="/submit-profile" className="submit-link">
           <img src={FilmmakingImg} alt="Filmmaking" />
           <span className="grid-text">FILMMAKING</span>
+          </Link>
         </div>
+        
+        
         <div className="grid-item music">
+        <Link to="/submit-profile" className="submit-link">
           <img src={MusicImg} alt="Music" />
           <span className="grid-text">MUSIC</span>
+          </Link>
         </div>
+    
       </div>
     </div>
   );
